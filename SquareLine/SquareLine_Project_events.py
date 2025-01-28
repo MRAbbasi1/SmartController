880669a5b99dc203c5ffc423c4ecd94765c949a6
MRAbbasi1 <mrabbasiu@gmail.com>
2025-01-28 14:32:18 +0330

    dev v0.37 merge

---
+++ SquareLine/SquareLine_Project_events.py
@@ -0,0 +1,52 @@
+
+def mainScreen(event_struct):
+    return
+
+
+def settingScreen(event_struct):
+    return
+
+
+def turnOffCtrl_Checked(event_struct):
+    return
+
+
+def turnOffCtrl_Unchecked(event_struct):
+    return
+
+
+def satusScreen(event_struct):
+    return
+
+
+def show_QR_code(event_struct):
+    return
+
+
+def processSettingScreen(event_struct):
+    return
+
+
+def submit_process_setting(event_struct):
+    return
+
+
+def addvancedSettingScreen(event_struct):
+    return
+
+
+def submit_advance_setting(event_struct):
+    return
+
+
+def searchingSensors(event_struct):
+    return
+
+
+def goToFactoryReseting(event_struct):
+    return
+
+
+def bootScreen(event_struct):
+    return
+
